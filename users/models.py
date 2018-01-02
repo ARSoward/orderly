7ca408364ccc61c7d6c60e8a64f5e342eee6d6c5
Abigail Soward <ARSoward@gmail.com>
2018-01-02 14:09:02 -0700

    updated user view and model

--- users/models.py
+++ users/models.py
@@ -6,19 +6,27 @@ from orders import models as orderModels #link to objects from another app in sa
 from django.utils.text import slugify
       
 class Picture(models.Model):
-  image = models.ImageField()
+  image = models.ImageField(upload_to = 'user-uploads/')
   caption = models.TextField(null=True, default=None)
   uploaded = models.DateTimeField('uploaded on', default=datetime.now)
   
 class Account(models.Model):
-    isPremium = models.BooleanField(default=False)
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='account')
-    about = models.TextField(max_length=600, null=True, default=None)
-    slug = models.SlugField(unique=True, default=slugify(user))
-    address = models.TextField(null=True, default=None)
-    website = models.TextField(null=True, default=None)
-    phone = models.TextField(null=True, default=None)
-    business = models.OneToOneField(orderModels.Business, related_name='account', null=True, on_delete=models.CASCADE)
-    picture = models.OneToOneField(Picture, related_name='owner', null=True, on_delete=models.CASCADE)
-    def __str__(self):
-      return '%s, %s' % (self.user.first_name, self.business.business_name)
+  business = models.OneToOneField(orderModels.Business, related_name='account', null=True, on_delete=models.CASCADE)
+  def makeSlug(instance):
+    slug = orig = slugify(instance.business)[:50]
+    for x in itertools.count(1):
+      if not Account.objects.filter(slug=instance.slug).exists():
+        break
+      instance.slug = "%s-%d" % (orig[:max_length - len(str(x)) - 1], x)
+    return slug
+  isPremium = models.BooleanField(default=False)
+  user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='account')
+  about = models.TextField(max_length=600, null=True, default=None)
+  slug = models.SlugField(unique=True, default=makeSlug)
+  address = models.TextField(null=True, default=None)
+  website = models.TextField(null=True, default=None)
+  phone = models.TextField(null=True, default=None)
+  picture = models.OneToOneField(Picture, related_name='owner', null=True, on_delete=models.CASCADE)
+  def __str__(self):
+    return '%s, %s' % (self.user.first_name, self.business.business_name)
+
--- users/views.py
+++ users/views.py
@@ -3,24 +3,22 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
-from .forms import AccountForm
+from .forms import UserForm
 from .models import Account
   
 def registerUser(request):
   if request.user.is_authenticated:
-    return redirect('/orders/')
-  if request.method == 'POST':
-    form = AccountForm(request.POST)
-    if form.is_valid():
-      form.save()
-      username = form.cleaned_data.get('username')
-      raw_password = form.cleaned_data.get('password1')
-      user = authenticate(username=username, password=raw_password)
-      login(request, user)
-      account = Account.objects.create(user=username, business = form.cleaned_data.get('account'))
-      return redirect('/settings/')
+    return redirect('/')
+  form = UserForm(request.POST or None)
+  if (request.method == 'POST' and form.is_valid()):
+    form.save()
+    username = form.cleaned_data.get('username')
+    raw_password = form.cleaned_data.get('password1')
+    user = authenticate(username=username, password=raw_password)
+    login(request, user)
+    account = Account.objects.create(user=username, business = form.cleaned_data.get('account'))
+    return redirect('/settings/')
   else:
-    form = AccountForm()
     return render(request, 'registration/registration.html', {'form': form})
   #context = {'form': AccountForm()}
   #return render(request, 'registration/registration.html',context)
