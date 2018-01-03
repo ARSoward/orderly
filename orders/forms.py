ade6ea3a6a3fd2669314de31cabf2ef40e0e2f79
Abigail Soward <ARSoward@gmail.com>
2018-01-02 17:54:21 -0700

    made orderlist mobile friendly

--- orders/forms.py
+++ orders/forms.py
@@ -29,10 +29,12 @@ class OrderItemForm(forms.ModelForm):
 #OrderItem Formset Factories          
 OrderItemSet = forms.inlineformset_factory(Order, OrderItem, form=OrderItemForm, min_num=1, validate_min=True, max_num=15, extra=5, can_delete=True)
 OrderItemModelSet = forms.modelformset_factory(OrderItem, fields = ['product', 'quantity', 'filled'], 
-                                                          widgets = {'product': Select2Widget,
-                                                            'filled': forms.CheckboxInput(attrs={'onchange': 'this.form.submit();'}),
-                                                            'quantity' : forms.TextInput(attrs={'onchange': 'this.form.submit();'})},
-                                                          extra=0)
+                                              widgets = {'product': Select2Widget(attrs={'class': 'form-control col-xs-12'}),
+                                                'filled': forms.CheckboxInput(attrs={'class': 'form-control', 
+                                                                                     'onchange': 'this.form.submit();'}),
+                                                'quantity' : forms.TextInput(attrs={'class': 'form-control',
+                                                                                    'onchange': 'this.form.submit();'})},
+                                              extra=0)
     
 #business form:
 # vendors add new customers.
